d4ebccf179869d551bff8d76e7b0e0aa4585469c
BSN_KKasahara <kohei.kasahara@bsnnet.co.jp>
2025-07-03 15:14:06 +0900

    test3

--- Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
+++ Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c5ed6576026acde5767f0dd77ece974038e5be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c5c01b102bb9ac34352b78fd6f8cf9c6db48ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
